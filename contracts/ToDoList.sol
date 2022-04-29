2d7f7d3cb3a97442029d9eca3e419354d36ff674
Mihir <mihir39nr@gmail.com>
2022-04-29 18:26:24 +0530

    Second Commit

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -3,4 +3,21 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract ToDoList{
     uint public taskcount = 0;  //State Variable !
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+    
+    constructor() public {
+        createTask("Nothing Here! Lol");
+    }
+    function createTask(string memory _content) public 
+    {
+        taskcount ++;
+        tasks[taskcount] = Task(taskcount,_content,false);
+    }
 }
+
